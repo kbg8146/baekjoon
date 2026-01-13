1ecfbffb3d6cb18c6cb658a50d0b0fc33f2ddd08
kbg8146 <kbg8146@gmail.com>
2026-01-13 21:32:28 +0900

    boj: 1978 cal

--- 1978/main.cpp
+++ 1978/main.cpp
@@ -2,12 +2,27 @@
 
 using namespace std;
 
-int N;
+
 
 int main(){
 
+    int N,num,ans=0;
     cin >> N;
+    for(int i=0;i<N;i++){
+        cin >> num;
 
+        if(num<2) continue;
 
+        bool isPrime = true;
+        for(int j=2;j*j<=num;j++){
+            if(num%j==0){
+                isPrime = false;
+                break;
+            }
+        }
+        
+        if(isPrime) ans++;
+    }
+    cout << ans;
     return 0;
 }
--- readme.txt
+++ readme.txt
@@ -1,3 +1,8 @@
 boj: 2178 bfs shortest path
 boj: 2667 dfs connected components
 boj: 15686 brute-force chicken distance
+
+
+git add 1978/main.cpp
+git commit -m "boj: 1978 solve"
+git push
