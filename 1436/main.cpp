5bb37163a9674c251b9e160b7cc5040632896e29
kbg8146 <kbg8146@gmail.com>
2026-01-19 15:37:43 +0900

    boj: 1436

---
+++ 1436/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        if(i==6){
+
+        }
+        else if(i==66){
+            
+        }
+        else if(i==666){
+
+        }
+    }
+    return 0;
+}
