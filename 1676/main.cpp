af58c248b06b1ed7135d7a4e9291d29cd002f5e0
kbg8146 <kbg8146@gmail.com>
2026-01-20 12:33:26 +0900

    boj: 1676 math

--- 1676/main.cpp
+++ 1676/main.cpp
@@ -5,16 +5,15 @@ using namespace std;
 int main(){
 
     int n;
-    long res=1;
     int cnt=0;
     cin >> n;
-    for(int i=1;i<=n;i++){
-        res *=i;
-    }
-    cout << res << endl;
-    string s = to_string(res);
-    for(int i=0;i<s.size();i++){
-        if(s[i]=='0') cnt++;
+    for(int num=1;num<=n;num++){
+        int x=num;
+        // 5의 배수
+        while(x%5==0){ 
+            cnt++;
+            x = x/5;
+        }
     }
 
     cout << cnt;
