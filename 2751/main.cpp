4a8ec0cbe3edc2ab757269a064f76db6f298cda2
kbg8146 <kbg8146@gmail.com>
2026-01-20 13:32:02 +0900

    boj: 2751 sort

---
+++ 2751/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[1000001];
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+
+    sort(a,a+n);
+
+    for(int i=0;i<n;i++){
+        cout << a[i]<<"\n";
+    }
+    return 0;
+}
