34e842758b6e42bcb805faaf3b562a27236a9a75
kbg8146 <kbg8146@gmail.com>
2026-01-22 16:11:38 +0900

    boj: 9012 2for

---
+++ 9012/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    string vps;
+    int cnt=0;
+    bool state = 1;
+    for(int i=0;i<n;i++){
+        cin >> vps;
+        cnt=0;
+        for(int j=0;j<vps.size();j++){
+            if(vps[j]=='('){
+                cnt++;
+            }
+            else if(vps[j]==')'){
+                cnt--;
+            }
+            if(cnt<0) state=0;
+        }
+        if(cnt!=0) state=0;
+        
+        if(state) cout << "YES" <<"\n";
+        else cout << "NO" << "\n";
+        state=1;
+    }
+
+
+    return 0;
+}
