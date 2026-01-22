6037c4e3f00d49dd07727bf7113209824a78ec81
kbg8146 <kbg8146@gmail.com>
2026-01-22 14:07:33 +0900

    boj: 2164 queue

---
+++ 2164/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    queue<int> q;
+    int x=0;
+
+    for(int i=1;i<=n;i++){
+        q.push(i);
+    }
+    
+    while(q.size()!=1){
+        q.pop();
+        x=q.front();
+        q.push(x);
+        q.pop();
+    }
+
+    cout << q.front();
+
+    return 0;
+}
