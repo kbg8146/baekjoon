6b16c47c76deab3768c4d3496886d03460ef741d
Bugeon Kim <kbg8146@gmail.com>
2026-01-23 23:29:39 +0900

    boj: 10816 sort, binary search

--- 10816/main.cpp
+++ 10816/main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
-#include <map>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n;
     cin >> n;
-    map<int,int> a;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin >> v[i];
+    }
+    sort(v.begin(),v.end());
     
+    int m;
+    cin >> m;
+    int num=0;
+    for(int i=0;i<m;i++){
+        cin >> num;
+        int l = lower_bound(v.begin(),v.end(),num)-v.begin();
+        int r = upper_bound(v.begin(),v.end(),num)-v.begin();
+        cout << r-l << " ";
+    }
+
     
 
     return 0;
