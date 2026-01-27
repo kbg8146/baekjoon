c3d5772406a2ea6cb4df9d0ed976a9dcd2d72307
김부건 <kbg8146@gmail.com>
2026-01-27 14:30:05 +0900

    boj: 11866 deque

--- 11866/main.cpp
+++ 11866/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <deque>
 
 using namespace std;
 
@@ -7,8 +8,31 @@ int main(){
     int n,k;
     cin >> n;
     cin >> k;
-
+    deque<int> dq;
     
+    for(int i=1;i<=n;i++){
+        dq.push_back(i);
+    }
+
+    int x=0;
+    for(int i=1;i<=n;i++){
+
+        for(int j=0;j<k-1;j++){
+            x = dq.front();
+            dq.pop_front();
+            dq.push_back(x);
+        }
+        x = dq.front();
+        if(i==1){
+            cout << "<" << x;
+        }
+        else {
+            cout << ", " << x;
+        }
+        dq.pop_front();
+
+    }
+    cout << ">";
 
     return 0;
 }
